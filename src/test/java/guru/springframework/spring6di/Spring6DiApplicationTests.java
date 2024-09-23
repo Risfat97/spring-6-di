4e2dbbbc258ab1bb39591824ce95a4b9ba8e35f8
Risfat97 <tamsir97@gmail.com>
2024-09-23 12:12:32 +0000

    Test getting beans from the Spring Application context

--- src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -1,10 +1,30 @@
 package guru.springframework.spring6di;
 
+import guru.springframework.spring6di.controller.HelloController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class Spring6DiApplicationTests {
+    @Autowired
+    private ApplicationContext ctx;
+
+    @Autowired
+    private HelloController helloController;
+
+    @Test
+    public void testGettingControllerFromContext() {
+        HelloController helloCtrl = ctx.getBean(HelloController.class);
+
+        System.out.println(helloCtrl.sayHello());
+    }
+
+    @Test
+    public void testAutowiredController() {
+        System.out.println(helloController.sayHello());
+    }
 
     @Test
     void contextLoads() {
